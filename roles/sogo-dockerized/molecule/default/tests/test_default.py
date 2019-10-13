2195467b50289bf247fb125f51dfbc6f40823736
Ilya Lesikov <bititanc@gmail.com>
2019-10-13 09:49:40 +0300

    upd

--- roles/sogo-dockerized/molecule/default/tests/test_default.py
+++ roles/sogo-dockerized/molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 import testinfra.utils.ansible_runner
 
@@ -7,37 +8,54 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-# def test_hosts_file(host):
-#     f = host.file('/etc/hosts')
-
-#     assert f.exists assert f.user == 'root'
-#     assert f.group == 'root'
-
-
-def test_docker_up(host):
+def test_docker_service_up(host):
     docker = host.service('docker')
     assert docker.is_running
     assert docker.is_enabled
 
 
-def test_mariadb_up(host):
+def test_sogo_net_created(host):
+    with host.sudo():
+        assert host.run_test('docker network inspect sogo_network')
+
+
+def test_mariadb_container_up(host):
     with host.sudo():
         mariadb_containers = host.docker.get_containers(
             name='mariadb', status='running'
         )
     assert len(mariadb_containers) == 1
 
-    with host.sudo():
-        mariadb_ip = mariadb_containers[0].inspect()['NetworkSettings']['IPAddress']
-    assert host.addr(mariadb_ip).port(3306).is_reachable
+
+def test_mariadb_port_reachable(host):
+    retries = range(20)
+    for retry in retries:
+        try:
+            assert host.addr('172.10.70.2').port(3306).is_reachable
+        except AssertionError:
+            if retry == retries[-1]:
+                raise
+            else:
+                time.sleep(1)
+                continue
+        break
 
 
-def test_sogo_up(host):
+def test_sogo_container_up(host):
     with host.sudo():
         sogo_containers = host.docker.get_containers(name='sogo', status='running')
     assert len(sogo_containers) == 1
 
-    with host.sudo():
-        sogo_ip = sogo_containers[0].inspect()['NetworkSettings']['IPAddress']
-    assert host.addr(sogo_ip).port(80).is_reachable
-    # pytest.raises(KeyError):
+
+def test_sogo_port_reachable(host):
+    retries = range(20)
+    for retry in retries:
+        try:
+            assert host.addr('172.10.70.3').port(80).is_reachable
+        except AssertionError:
+            if retry == retries[-1]:
+                raise
+            else:
+                time.sleep(1)
+                continue
+        break
